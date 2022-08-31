d44be43360914e0c9c293f81c876629b1de24b6d
kouteimonev <58534489+kouteimonev@users.noreply.github.com>
2022-08-31 00:33:36 -0500

    Update Problema_7.java

--- Problema_7.java
+++ Problema_7.java
@@ -1,26 +1,23 @@
 import java.util.*;
 
 public class Problema_7 {
-
-	Scanner leer = new Scanner(System.in);
 	
-	int pedir() {
-		System.out.println("Ingrese un numero entero:");
+	public static int pedir() {
+		System.out.println("Ingrese un numero entero:\n");
+		Scanner leer = new Scanner(System.in);
 		return leer.nextInt();
 	}
 	
-	int triple(int i) {
+	public static int triple(int i) {
 		return 3*i;
 	}
 	
-	public void main(String[] args) {
-		int num;
+	public static void main(String[] args) {
+		int num, triple;
 		
 		num = pedir();
-		System.out.println(num);
+		triple = triple(num);
 		
-		System.out.println("Ingrese un numero:");
-		num = triple(leer.nextInt());
-		System.out.println(num);
+		System.out.println("El triple del numero es " + triple);
 	}
 }
